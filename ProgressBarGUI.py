81e0a8f802350a8be651d399deba6aa34e71c50d
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2019-05-13 18:28:15 +0200

    Update ProgressBarGUI.py

--- ProgressBarGUI.py
+++ ProgressBarGUI.py
@@ -1,50 +1,52 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QProgressBar, QPushButton, QApplication, QDialog
+import os
+from PyQt5.QtWidgets import QWidget, QProgressBar, QPushButton, QApplication
 from PyQt5.QtCore import QBasicTimer
-from interfacefinalGUI import Ui_Dialog
-
+import interfacefinalGUI
 
 class ProgressBarDemo(QWidget):
+                
+	def __init__(self):
+		super().__init__()
 
-    def __init__(self):
-        super().__init__()
+		self.progressBar = QProgressBar(self)
+		self.progressBar.setGeometry(30, 40, 200, 25)
 
-        self.progressBar = QProgressBar(self)
-        self.progressBar.setGeometry(30, 40, 200, 25)
+		self.btnStart = QPushButton('Start SIR', self)
+		self.btnStart.move(30, 90)
+		self.btnStart.clicked.connect(self.startProgress) 
 
-        self.btnStart = QPushButton('Start SIR', self)
-        self.btnStart.move(30, 90)
-        self.btnStart.clicked.connect(self.startProgress)
-        self.hasBeen = False
-        self.timer = QBasicTimer()
-        self.step = 0
+		
+		self.timer = QBasicTimer()
+		self.step = 0
 
-    def startProgress(self):
-        if self.hasBeen == True:
-            self.btnStart.setText('SIR will launch')
-            app1 = QApplication(sys.argv)
-            Dialog = QDialog()
-            ui = Ui_Dialog()
-            ui.setupUi(Dialog)
-            Dialog.show()
-            self.destroy()
-            sys.exit(app1.exec_())
-        self.timer.start(100, self)
-        self.hasBeen = True
-    def timerEvent(self, event):
-        if self.step >= 100:
-            self.timer.stop()
-            self.btnStart.setText('Click here')
-            return
+	
 
-        self.step += 1
-        self.progressBar.setValue(self.step)
+	def startProgress(self):
+		if self.timer.isActive():
+			self.timer.stop()
+			self.btnStart.setText('Start SIR')
+		else:
+			self.timer.start(100, self)
+			self.btnStart.setText('SIR will launch')
 
+	def timerEvent(self, event):
+		if self.step >= 100:
+			self.timer.stop()
+			self.btnStart.setText('Click here')
+			os.system('interfacefinalGUI.py')
+			return
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
+		self.step +=1
+		self.progressBar.setValue(self.step)
 
-    demo = ProgressBarDemo()
-    demo.show()
+                
 
-    sys.exit(app.exec_())
+if __name__=='__main__':
+	app = QApplication(sys.argv)
+
+	demo = ProgressBarDemo()
+	demo.show()
+
+	sys.exit(app.exec_())
+
